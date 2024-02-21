c8981004bf9dfe60056ae48ba87b21adbb722af9
Mohssine <mohssineaitboukdir@gmail.com>
2024-02-21 16:10:59 +0100

    first commit

---
+++ api.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+
+// const url = 'https://api.themoviedb.org/3/authentication';
+const url = 'https://api.themoviedb.org/3/movie/changes?page=1';
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTNlYTNkNGY1OWM1NTllZjk3NTE2YmVkNDhkMGQ4YyIsInN1YiI6IjY1ZDYwMjc1MzNhMzc2MDE4Njc4M2YzMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zy0z1Sub0defogGBge8nVrMc44ucJ_sog0kfKt9Idfg'
+  }
+};
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error('error:' + err));
+
+  
+// -Get a movie by id
+const url2  = 'https://api.themoviedb.org/3/movie/1229760';
+
+
+fetch(url2, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error('error:' + err));
